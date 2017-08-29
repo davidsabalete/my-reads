ab1778534286306080a4fb0997da13c0dbd9f67f
David Sabalete <davidsabalete@capitole-consulting.com>
2017-08-29 06:25:20 +0000

    Add Bookshelf component

---
+++ src/Bookshelf.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+class Bookshelf extends Component {
+    
+}
+
+export default Bookshelf
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import * as BooksAPI from './BooksAPI'
 
 class ListBooks extends Component {
     render () {
