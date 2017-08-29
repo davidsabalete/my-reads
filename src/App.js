0eb29acc40c613c2b065d5e6498f7d194f3b84ad
David Sabalete <davidsabalete@capitole-consulting.com>
2017-08-29 10:27:38 +0000

    Add Book component

--- src/App.js
+++ src/App.js
@@ -1,52 +1,62 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import ListBooks from './ListBooks'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
     books: []
+  }
+
+  componentDidMount() {
+    // once component has been mounted, get list of books
+    this.getBooks()
+  }
+
+  getBooks = () => {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+      console.log(books)
+    })
+  }
+
+  updateBook = (book, shelf) => {
+    BooksAPI.update(book, shelf).then((b) => {
+      this.getBooks()
+    })
   }
 
   render() {
     return (
       <div className="app">
       
-        <Route path="/search" render={() => (
+        <Route path="/search" render={({ history }) => (
           <div className="search-books">
             <div className="search-books-bar">
               <Link className="close-search" to="/">Close</Link>
               <div className="search-books-input-wrapper">
-                {/* 
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
                 <input type="text" placeholder="Search by title or author"/>
-                
               </div>
             </div>
             <div className="search-books-results">
               <ol className="books-grid"></ol>
             </div>
           </div>
-        )} />
+        )}/>
         
-        <Route exact path="/" render={({ history }) => (
-          <ListBooks 
-            books={this.state.books}
+        <Route exact path="/" render={() => (
+          <div>
+            <ListBooks 
+              books={ this.state.books }
+              onUpdateBook={ this.updateBook }
             />
-        )} />
+            <div className="open-search">
+                <Link to="/search">Add a book</Link>
+            </div>
+          </div>
+        )}/>
+        
       </div>
     )
   }
--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -4,7 +4,21 @@ import PropTypes from 'prop-types'
 function Bookshelf(props) {
     return (
         <div className="bookshelf">
-            <h2 className="bookshelf-title">{ props.title }</h2>
+            <h2 className="bookshelf-title">
+                { props.title }
+            </h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                {props.books.map((book) =>(
+                    <li key={book.id} >
+                      <Book 
+                        data={book}
+                        onUpdateBook={props.onUpdateBook}
+                      ></Book>
+                    </li>
+                  ))}
+                </ol>  
+            </div>
         </div>
     )
 }
@@ -12,7 +26,6 @@ function Bookshelf(props) {
 Bookshelf.propTypes = {
     title: PropTypes.string.isRequired,
     onUpdateBook: PropTypes.func.isRequired
-
 }
 
 export default Bookshelf
